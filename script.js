bc1271540e56809563d7389676a3838dd7b8f87e
Rafael Almeida <148403445+rrafaum@users.noreply.github.com>
2026-08-07 09:12:38 -0300

    feat: Adicionar lógica da rodada - Adicionada a lógica para uma única rodada

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getComputerChoice() {
     const computerChoice = Math.floor(Math.random() * 3) + 1;
 
-    switch (choice) {
+    switch (computerChoice) {
         case 1:
             return 'Rock';
             break;
@@ -15,7 +15,43 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    const humanChoice = prompt('Choose: Rock, Paper or Scissors', '');
+    let humanChoice = prompt('Choose: Rock, Paper or Scissors', '');
+
+    humanChoice = humanChoice.toUpperCase().slice(0,1)
+    + humanChoice.toLowerCase().slice(1);
 
     return humanChoice;
 }
+
+function playRound(humanChoice, computerChoice) {
+
+    if (humanChoice === computerChoice) {
+
+        console.log(`Draw! \nYour choice: ${humanChoice} | Computer choice: ${computerChoice}
+            You: ${humanScore} | Computer: ${computerScore}`);
+
+    } else if (
+        (humanChoice === 'Rock' && computerChoice === 'Scissors') ||
+        (humanChoice === 'Paper' && computerChoice === 'Rock') ||
+        (humanChoice === 'Scissors' && computerChoice === 'Paper')
+    ) {
+
+        humanScore++;
+        console.log(`Your win! \nYour choice: ${humanChoice} | Computer choice: ${computerChoice}
+            You: ${humanScore} | Computer: ${computerScore}`);
+
+    } else {
+        computerScore++;
+        console.log(`Your lost! \nYour choice: ${humanChoice} | Computer choice: ${computerChoice}
+            You: ${humanScore} | Computer: ${computerScore}`);
+    }
+
+}
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+let humanScore = 0;
+let computerScore = 0;
+
+playRound(humanChoice, computerChoice);
